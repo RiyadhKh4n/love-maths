fb1d8a538a8800f239ab54a75f9fdd1382de5656
Arkay <riyadh.k@outlook.com>
2021-10-10 16:41:00 +0000

    Add event listener for keydown

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,12 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event){
+        if(event.key === "Enter"){
+            checkAnswer();
+        }
+    });
+
     runGame("addition");
 
 });
@@ -21,6 +27,9 @@ document.addEventListener("DOMContentLoaded", function () {
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus(); //keeps focus on answer box
 
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
@@ -129,5 +138,5 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivisionQuestion(operand1, operand2){
-    
+
 }
